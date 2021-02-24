3ef692b5e683ca9ed58f09a1bd4b1f77d8c6bc3d
Tim Juretzek <tj@jur-solutions.de>
2021-02-24 12:54:03 +0100

    added smooth scroll

--- src/components/ImageCard/index.js
+++ src/components/ImageCard/index.js
@@ -28,6 +28,14 @@ class ImageCard extends PureComponent {
     this.setState({ now })
   }
 
+  scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    })
+  }
+
   render() {
     const { id, link, title, end, leaderboardItem, proposer } = this.props
     const { now } = this.state
@@ -36,12 +44,26 @@ class ImageCard extends PureComponent {
     return (
       <div className="card">
         <CardTimeBar elapsed={end - now} />
-        <Link to={`/details/${id}`} replace className="card-id-link">
+        <Link
+          to={`/details/${id}`}
+          replace
+          className="card-id-link"
+          onClick={() => {
+            this.scrollTop()
+          }}
+        >
           <div className="card-id">
             <span>#{id}</span>
           </div>
         </Link>
-        <Link to={`/details/${id}`} replace className="card-title-link">
+        <Link
+          to={`/details/${id}`}
+          replace
+          className="card-title-link"
+          onClick={() => {
+            this.scrollTop()
+          }}
+        >
           <h3 className="card-title">{title ? title : 'untitled'}</h3>
         </Link>
         {isVideo ? (
@@ -49,7 +71,14 @@ class ImageCard extends PureComponent {
             <ReactPlayer url={link} width="100%" controls={true} />
           </div>
         ) : (
-          <Link to={`/details/${id}`} replace className="card-link">
+          <Link
+            to={`/details/${id}`}
+            replace
+            className="card-link"
+            onClick={() => {
+              this.scrollTop()
+            }}
+          >
             <img className="img card-image" src={link} alt="meme" />
           </Link>
         )}
